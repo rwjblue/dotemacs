db955b33910eee4a8f3c8555ac3ae031d0224328
Robert Jackson <robertj@promedicalinc.com>
2012-10-25 17:28:35 -0400

    Update backup/autosave options.

--- config/rwj-backup.el
+++ config/rwj-backup.el
@@ -1,5 +1,10 @@
 (setq backup-directory-alist `((".*" . ,rwj-backup-dir)))
-(setq auto-save-file-name-transforms `((".*" , rwj-backup-dir t)))
+(setq auto-save-file-name-transforms `((".*" ,rwj-backup-dir t)))
+
+(setq delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 5
+  version-control t)
 
 ;; saveplace remembers your location in a file when saving files
 (setq save-place-file (expand-file-name "saveplace" rwj-backup-dir))
