bdcd568265d1a3eee1c4c452a2f1909c4ff601c3
Robert Jackson <robertj@promedicalinc.com>
2012-10-26 18:10:30 -0400

    Take out el-get. Use standard packaging.

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 (setq rwj-emacs-config-dir (file-name-directory load-file-name))
 (setq rwj-backup-dir (expand-file-name "backups" rwj-emacs-config-dir))
 (setq rwj-config-dir (expand-file-name "config" rwj-emacs-config-dir))
+(setq rwj-vendor-dir (expand-file-name "vendor" rwj-emacs-config-dir))
 
 
 (unless (file-exists-p rwj-backup-dir)
@@ -10,29 +11,46 @@
 (setq custom-file
       (expand-file-name "emacs-customizations.el" rwj-emacs-config-dir))
 
+;; setup packages
 (require 'cl)
+(require 'package)
 
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+;(add-to-list 'package-archives
+;       '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+       '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+       '("ELPA" . "http://tromey.com/elpa/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
 
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
+;; required because of a package.el bug
+;(setq url-http-attempt-keepalives nil)
 
-(el-get 'sync)
+(when (not package-archive-contents)
+  (package-refresh-contents))
 
 ;; list of packages to ensure are loaded at launch
 (setq rwj-packages
       '(ack-and-a-half coffee-mode eieio exec-path-from-shell
-	flymake flymake-ruby flymake-shell
-	expand-region gh gist guru-mode haml-mode htmlize inflections
-	inf-ruby magit magithub multi-term org-plus-contrib rinari rvm
-	scss-mode yasnippet xml-rpc molokai-theme solarized-theme
-	zenburn-theme))
+  flymake flymake-ruby flymake-shell
+  expand-region gh gist guru-mode haml-mode htmlize inflections
+  inf-ruby magit magithub multi-term org-plus-contrib rinari rvm
+  scss-mode yasnippet xml-rpc molokai-theme solarized-theme
+  zenburn-theme))
 
-(el-get 'sync rwj-packages)
+(defun rwj-packages-installed-p ()
+  (loop for p in rwj-packages
+  when (not (package-installed-p p)) do (return nil)
+  finally (return t)))
+
+(unless (rwj-packages-installed-p)
+  ;; check for new packages (package versions)
+  (package-refresh-contents)
+  ;; install the missing packages
+  (dolist (p rwj-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
 
 ;; Load all elisp files in ./config
 (if (file-exists-p rwj-config-dir)
