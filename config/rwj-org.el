1489b427432e8f831626d28445a1a5d6f851bef8
Robert Jackson <robertj@promedicalinc.com>
2012-10-26 23:47:40 -0400

    Update org-mode config to handle key binding conflicts.

--- config/rwj-org.el
+++ config/rwj-org.el
@@ -2,6 +2,19 @@
 (require 'org-src)  ;; edit src inline
 (require 'htmlize)  ;; required for export
 
+(defun yas/org-very-safe-expand ()
+  (let ((yas/fallback-behavior 'return-nil)) (yas/expand)))
+
 (add-hook 'org-mode-hook
 	  (lambda ()
-	    (toggle-truncate-lines)))
+	    (toggle-truncate-lines)
+	    (make-variable-buffer-local 'yas/trigger-key)
+	    (setq yas/trigger-key [tab])
+	    (add-to-list 'org-tab-first-hook 'yas/org-very-safe-expand)
+	    (define-key yas/keymap [tab] 'yas/next-field)))
+
+;; Make windmove work in org-mode:
+(add-hook 'org-shiftup-final-hook 'windmove-up)
+(add-hook 'org-shiftleft-final-hook 'windmove-left)
+(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
